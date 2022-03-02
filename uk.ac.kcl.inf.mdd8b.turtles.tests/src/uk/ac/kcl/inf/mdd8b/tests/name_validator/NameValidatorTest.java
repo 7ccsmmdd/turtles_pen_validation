0ac2e2bd4d16b76f08bb8e688a253178844c07c5
Steffen Zschaler <szschaler@acm.org>
2022-03-02 12:06:12 +0000

    Initial renaming

---
+++ uk.ac.kcl.inf.mdd8b.turtles.tests/src/uk/ac/kcl/inf/mdd8b/tests/name_validator/NameValidatorTest.java
@@ -0,0 +1,11 @@
+package uk.ac.kcl.inf.mdd8b.tests.name_validator;
+
+
+import org.eclipse.xpect.runner.XpectRunner;
+import org.eclipse.xpect.xtext.lib.tests.XtextTests;
+import org.junit.runner.RunWith;
+
+@RunWith(XpectRunner.class)
+public class NameValidatorTest extends XtextTests {
+
+}
